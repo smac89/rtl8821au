ad575339fa86f30ed0a696eecd8c0e17293d4a1a
smac89 <smac89@users.noreply.github.com>
2018-12-21 21:34:22 -0600

    Check for root

--- scripts/kernels.sh
+++ scripts/kernels.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! $TRAVIS_SUDO; then
+    echo "Need root"
+    exit 1
+fi
+
 HEADERS=$(sudo apt-cache search '^linux-headers-[0-9]\.[0-9]+\.0-[0-9]+.+generic$' --names-only | sed -E 's/(.+?) \- .*/\1/')
 
 for linux_header in $HEADERS; do
