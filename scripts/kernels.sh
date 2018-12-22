ccaca6019727b5ab328c1cc0e8c5a2712d6cab0a
smac89 <smac89@users.noreply.github.com>
2018-12-21 22:07:35 -0600

    Only build the latest versions from each kernel

--- scripts/kernels.sh
+++ scripts/kernels.sh
@@ -5,7 +5,7 @@ if ! $TRAVIS_SUDO; then
     exit 1
 fi
 
-HEADERS=$(sudo apt-cache search '^linux-headers-[0-9]\.[0-9]+\.0-[0-9]+.+generic$' --names-only | sed -E 's/(.+?) \- .*/\1/')
+HEADERS=$(sudo apt-cache search '^linux-image-[0-9]\.[0-9]+\.0-[0-9]+.+generic$' --names-only | sort --version-sort --reverse | sed -r 's/(.+?) \- .*/\1/' | sort --field-separator='-' --ignore-case --unique --key=1,3)
 
 for linux_header in $HEADERS; do
     sudo apt install $linux_header -y
