5f10113d4fdf2994c9c14fb10f916a7fa403185e
smac89 <smac89@users.noreply.github.com>
2018-12-22 00:26:54 -0600

    Looks like I did not install the headers, but rather the images

--- scripts/kernels.sh
+++ scripts/kernels.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-HEADERS=$(apt-cache search '^linux-image-[0-9]\.[0-9]+\.0-[0-9]+.+generic$' --names-only | sort --version-sort --reverse | sed -r 's/(.+?) \- .*/\1/' | sort --field-separator='-' --ignore-case --unique --key=1,3)
+HEADERS=$(apt-cache search '^linux-headers-[0-9]\.[0-9]+\.0-[0-9]+.+generic$' --names-only | sort --version-sort --reverse | sed -r 's/(.+?) \- .*/\1/' | sort --field-separator='-' --ignore-case --unique --key=1,3)
 
 IGNORED_HEADERS="$(cat <<'EOF'
 EOF
@@ -10,7 +10,7 @@ EOF
 
 for linux_header in $HEADERS; do
     if ! [[ $IGNORED_HEADERS =~ "$linux_header" ]]; then
-        # apt --quiet install $linux_header --yes
+        apt --quiet install $linux_header --yes 1>&2
         echo "${linux_header//linux-headers-/}"
     fi
 done >> "$INSTALLED_HEADERS"
