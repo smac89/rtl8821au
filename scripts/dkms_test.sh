b3eaf789f25d5f6202c8afe5e1577b7eeba6ddf5
smac89 <smac89@users.noreply.github.com>
2018-12-22 00:44:08 -0600

    Removing ignored kernels and fixing the dksm installation so that it adds the sources before attempting to build

--- scripts/dkms_test.sh
+++ scripts/dkms_test.sh
@@ -15,5 +15,8 @@ done < 'dkms.conf'
 
 for linux_header in "$(cat $INSTALLED_HEADERS)"; do
     dkms status -k "$linux_header"
+
+    dkms add "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
+    dkms build "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
     dkms install "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
 done
