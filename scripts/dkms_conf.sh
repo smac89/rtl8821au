147dc2ba591a2b78322ce6728496b62053078495
smac89 <smac89@users.noreply.github.com>
2018-12-22 01:24:34 -0600

    Use readlink to read the files

--- scripts/dkms_conf.sh
+++ scripts/dkms_conf.sh
@@ -10,4 +10,4 @@ while IFS='=' read -r name value; do
         'DEST_MODULE_NAME[0]') DRV_MODNAME="$clean_value" ;;
         'BUILT_MODULE_NAME[0]') if [ -z "$DRV_MODNAME" ]; then DRV_MODNAME="$clean_value"; fi ;;
     esac
-done < '../dkms.conf'
+done < $(readlink -f '../dkms.conf')
