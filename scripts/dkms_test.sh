cd37f132a58d721a8bc4b41586bb69193382ace1
smac89 <smac89@users.noreply.github.com>
2018-12-23 01:34:15 -0600

    Ignore adding the module to the kernel if it is already there

--- scripts/dkms_test.sh
+++ scripts/dkms_test.sh
@@ -3,9 +3,9 @@
 set -e
 
 for linux_header in "$(cat $INSTALLED_HEADERS)"; do
-    dkms status -k "$linux_header"
+    dkms status "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
 
-    dkms add -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
+    dkms add -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header" || true
     dkms build -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
     dkms install -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
 done
