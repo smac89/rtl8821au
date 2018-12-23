fbe3f302a3adcb2aa2549d977b1116cdd7b1a57d
smac89 <smac89@users.noreply.github.com>
2018-12-23 00:28:39 -0600

    Add proper flags to dkms

--- scripts/dkms_test.sh
+++ scripts/dkms_test.sh
@@ -5,7 +5,7 @@ set -e
 for linux_header in "$(cat $INSTALLED_HEADERS)"; do
     dkms status -k "$linux_header"
 
-    dkms add "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
-    dkms build "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
-    dkms install "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
+    dkms add -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
+    dkms build -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
+    dkms install -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
 done
