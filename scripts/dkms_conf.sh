a14c894f047dcd0287b5d9ed7396f3235df55307
smac89 <smac89@users.noreply.github.com>
2018-12-23 01:16:47 -0600

    Export the variables

--- scripts/dkms_conf.sh
+++ scripts/dkms_conf.sh
@@ -1,13 +1,22 @@
 DRV_NAME=
 DRV_VERSION=
-DRV_MODNAME=
 
 while IFS='=' read -r name value; do
     clean_value="${value//\"/}"
     case "$name" in
         'PACKAGE_NAME') DRV_NAME="$clean_value" ;;
         'PACKAGE_VERSION') DRV_VERSION="$clean_value" ;;
-        'DEST_MODULE_NAME[0]') DRV_MODNAME="$clean_value" ;;
-        'BUILT_MODULE_NAME[0]') if [ -z "$DRV_MODNAME" ]; then DRV_MODNAME="$clean_value"; fi ;;
     esac
 done < "${1-$(cat)}" # filename from argument or read from stdin
+
+if ! [ -z "DRV_NAME" ]; then
+    export DRV_NAME
+else
+    echo "DRV_NAME is not set" 1>&2
+fi
+
+if ! [ -z "DRV_VERSION" ]; then
+    export DRV_VERSION
+else
+    echo "DRV_VERSION is not set" 1>&2
+fi
