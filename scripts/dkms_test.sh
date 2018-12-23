a2541281a8a37170fc4ce576a339794b2b650d8a
smac89 <smac89@users.noreply.github.com>
2018-12-23 02:27:16 -0600

    Remove quotes from bash loop

--- scripts/dkms_test.sh
+++ scripts/dkms_test.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-for linux_header in "$(cat $INSTALLED_HEADERS)"; do
+for linux_header in $(cat $INSTALLED_HEADERS); do
     dkms status "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header"
 
     dkms add -m "${DRV_NAME}/${DRV_VERSION}" -k "$linux_header" || true
